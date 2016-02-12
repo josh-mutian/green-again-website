9553a3812bfe7b9c5ba2d69f63d8524571444951
= <=>
2016-02-12 13:17:24 -0600

    Progress on homepage.

--- home.js
+++ home.js
@@ -1,9 +1,20 @@
 $(document).ready(function() {
     var view_height = $(window).height();
+    var view_width = $(window).height();
     $('.height-full').css({
         'height': view_height
     });
-    $('.half-height').css({
-        'height': view_height / 2
+    $('.height-half').css({
+        'height': view_height
+    })
+    // $('.stat-tryptic').css({
+    //     'width': view_width * .9,
+    //     'margin-left': view_width * .05,
+    //     'margin-right': view_width * .05
+    // });
+    $(window).resize(function() {
+        view_height = $(window).height();
+        $('.height-full').css('height', view_height)
+        $('.height-half').css('height', view_height / 2)
     })
 })
--- index.php
+++ index.php
@@ -24,6 +24,44 @@
             </form>
         </div>
     </div>
+
+    <div class='section-container'>
+        <p class='section-title'>The Problem</p>
+        <div class='stat-tryptic'>
+            <p class='stat-number'>3%</p>
+            <p class='stat-desc'>of the world's biodiversity is in Madagascar</p>
+        </div>
+        <div class='stat-tryptic'>
+            <p class='stat-number'>3%</p>
+            <p class='stat-desc'>of the world's biodiversity is in Madagascar</p>
+        </div>
+        <div class='stat-tryptic'>
+            <p class='stat-number'>3%</p>
+            <p class='stat-desc'>of the world's biodiversity is in Madagascar</p>
+        </div>
+    </div>
+
+    <div class='green-back, section-container' id='mission-statement'>
+        <p class='section-title'>Our Mission</p>
+        <p class='section-slogan'>We seek to replant an area of Madagascar's rainforest burned down by an unnatural forest fire as well as encourage cultural understanding through a real world demonstration of how to live sustainably</p>
+    </div>
+
+    <div class='section-container'>
+        <p class='section-title'>Our Impact</p>
+        <div class='stat-tryptic'>
+            <p class='stat-number'>3103</p>
+            <p class='stat-desc'>seedlings 1 year old</p>
+        </div>
+        <div class='stat-tryptic'>
+            <p class='stat-number'>1000+</p>
+            <p class='stat-desc'>germinations ready for pot</p>
+        </div>
+        <div class='stat-tryptic'>
+            <p class='stat-number'>4000+</p>
+            <p class='stat-desc'>1 year old in "wild nursery" on burned land ready for planting</p>
+        </div>
+    </div>
+
     <?php
         $nav_dir = $_SERVER['DOCUMENT_ROOT']."/footer/footer.php";
         include_once($nav_dir);
@@ -32,3 +70,6 @@
     <script src='/nav/nav.js'></script>
 </body>
 </html>
+
+
+
